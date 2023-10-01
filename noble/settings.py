0ac31999ae4734301224e9684f2b6d4cb0de7384
Vishnu021198 <nair654065@gmail.com>
2023-10-01 17:50:23 +0530

    Cart Count Context Processor Done Update 16.5

--- noble/settings.py
+++ noble/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'userapp.context_processor.product_links'
+                'userapp.context_processor.product_links',
+                'cartapp.context_processors.counter',
             ],
         },
     },
